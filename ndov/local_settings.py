2d3b316e8661a7b7b4f67b421bd9690b09affad5
Joel Haasnoot <j.haasnoot@deskbookers.com>
2012-02-24 22:47:47 +0100

    Move database settings to local_settings

---
+++ ndov/local_settings.py
@@ -0,0 +1,6 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/home/skinkie/sources/ndov-in-a-box/db.sqlite3',                      # Or path to database file if using sqlite3.
+    }
+}
